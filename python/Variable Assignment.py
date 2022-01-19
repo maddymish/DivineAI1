f92a844a1c945b4d18c8f909768dc24bff26221d
Madhuri Mishra <97845574+maddymish@users.noreply.github.com>
2022-01-19 15:22:55 +0530

    Add files via upload
    v

---
+++ python/Variable Assignment.py
@@ -0,0 +1,121 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+my_dogs = 2
+my_dogs
+
+
+# In[2]:
+
+
+my_dogs = ['Sammy', 'Frankie']
+my_dogs
+
+
+# In[3]:
+
+
+a = 5
+a
+
+
+# In[4]:
+
+
+a = 10
+a
+
+
+# In[5]:
+
+
+a + a
+a
+
+
+# In[7]:
+
+
+a=a+10
+a
+
+
+# In[8]:
+
+
+a*=2
+a
+
+
+# In[10]:
+
+
+type(a)
+
+
+# In[11]:
+
+
+a=(1,2)
+type(a)
+
+
+# In[12]:
+
+
+my_income = 100
+tax_rate = 0.1
+my_taxes = my_income*tax_rate
+my_taxes
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
